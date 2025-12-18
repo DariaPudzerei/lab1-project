c7c513bd760ffda1a214587559c29a358a993785
Vladyslav <boiko.vladyslav1123@vu.cdu.edu.ua>
2025-12-18 20:43:45 +0200

    Add C# console application with Hello World

---
+++ src/MyLabProject/MyLabProject/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace MyLabProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello! It is a new project!!!");
+            Console.ReadLine();
+        }
+    }
+}
